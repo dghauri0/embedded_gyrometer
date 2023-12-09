1a4a66ad0fd23fb6e80bcafbdd20d95f80da205c
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-12-09 05:03:53 -0500

    ideating code styles and configurations

--- embedded_gyro_pio/src/main.cpp
+++ embedded_gyro_pio/src/main.cpp
@@ -1,5 +1,31 @@
 #include <mbed.h>
 
+/* Gyroscope Control Register Addresses */
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+
+/* Gyroscope Control Register Configurations */
+
+// CTRL_REG1
+// +-----+-----+-----+-----+----+-----+-----+-----+
+// | DR1 | DR0 | BW1 | BW0 | PD | Zen | Yen | Xen |
+// +-----+-----+-----+-----+----+-----+-----+-----+
+// | 0   | 1   | 1   | 0   | 1  | 1   | 1   | 1   |
+// +-----+-----+-----+-----+----+-----+-----+-----+
+// Sets output data rate (ODR) = 200 Hz, Cutoff = 50.
+// Gyroscope set to normal operating mode (power down mode disabled).
+// All axes enabled.
+#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
+
+// 
+#define CTRL_REG3_CONFIG 0b0'0'0'0'1'000
+
+//
+#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
+
+
 int main() {
     return 0;
 }
