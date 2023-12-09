c5cc8ba1320cda7e1c9dfaae292141017766d1f6
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-12-09 05:53:11 -0500

    Update main.cpp

--- embedded_gyro_pio/src/main.cpp
+++ embedded_gyro_pio/src/main.cpp
@@ -19,13 +19,38 @@
 // All axes enabled.
 #define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
 
-// 
+// CTRL_REG3
+// +---------+---------+-----------+-------+---------+--------+---------+----------+
+// | I1_Int1 | I1_Boot | H_Lactive | PP_OD | I2_DRDY | I2_WTM | I2_ORun | I2_Empty |
+// +---------+---------+-----------+-------+---------+--------+---------+----------+
+// | 0       | 0       | 0         | 0     | 1       | 0      | 0       | 0        |
+// +---------+---------+-----------+-------+---------+--------+---------+----------+
+// Enable interrupt 2 to assert when data is ready.
 #define CTRL_REG3_CONFIG 0b0'0'0'0'1'000
 
-//
+// CTRL_REG4
+// +---+-----+-----+-----+---+-----+-----+-----+
+// | 0 | BLE | FS1 | FS0 | - | ST1 | ST0 | SIM |
+// +---+-----+-----+-----+---+-----+-----+-----+
+// | 0 | 0   | 0   | 1   | 0 | 0   | 0   | 0   |
+// +---+-----+-----+-----+---+-----+-----+-----+
+// Little endian mode. LSB @ lower address.
+// Set full scale selection to 500 dps.
 #define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
 
 
+
 int main() {
+
+    /* SPI Initialization */
+
+    // PF_9 --> Gyroscope SPI MOSI Pin
+    // PF_8 --> Gyroscope SPI MISO Pin
+    // PF_7 --> Gyroscope SPI Clock Pin
+    // Using GPIO SSEL Line.
+    SPI spi(PF_9, PF_8, PF_7, PC_1, use_gpio_ssel);
+
+    
+
     return 0;
 }
